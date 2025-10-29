78380433f856731243449413f218756d79f60425
Amila <amila.residovic@fet.ba>
2025-10-29 15:08:23 +0100

    Dodana klasa Programer

--- HW1/ZadatakA/src/main/Main.kt
+++ HW1/ZadatakA/src/main/Main.kt
@@ -2,3 +2,26 @@ interface Osoba{
     fun getImePrezime():String
     fun getZemlja():String
 }
+
+open class Programer(val ime:String,val prezime:String,val godineIskustva:Int,val oznakaZemlje:String,val programskiJezici:List<String>):Osoba{
+    init{
+        if(ime.isEmpty()){
+            throw Exception("Ime ne smije biti prazno")
+        }
+        if(godineIskustva<0){
+            throw Exception("Broj godina ne smije biti negativan")
+        }
+        if(programskiJezici.isEmpty()){
+            throw Exception("Lista jezika ne smije biti prazna")
+        }
+    }
+
+    val jezici:List<String> = programskiJezici.map{it.lowercase()}
+
+    override fun getImePrezime(): String {
+        return "$ime $prezime"
+    }
+    override fun getZemlja(): String {
+        return oznakaZemlje
+    }
+}
