a2701929ef8d2205c66b4fee37a711b501ed7941
Amila <amila.residovic@fet.ba>
2025-10-30 16:23:52 +0100

    Dodano prosjecno iskustvo

--- HW1/ZadatakA/src/main/Main.kt
+++ HW1/ZadatakA/src/main/Main.kt
@@ -39,6 +39,7 @@ fun analizaJezikaVerzija1(programeri:List<Programer>):Map<String,Int>{
 }
 fun analizaJezikaVerzija2(programeri: List<Programer>):Map<String,Int> {
     val mapa = mutableMapOf<String, Int>()
+
     for(i in programeri){
         for(j in i.jezici){
             mapa[j]=mapa.getOrDefault(j,0)+1
@@ -46,5 +47,31 @@ fun analizaJezikaVerzija2(programeri: List<Programer>):Map<String,Int> {
     }
     return mapa
 }
+
+//Za prosjecnoIskustvoVerzija1 sam koristila Chat GPT kako bih vidjela na koji nacin kreirati
+// podatke kao parove i kako koristiti mapValues za izracun prosjeka
+
+fun prosjecnoIskustvoVerzija1(programeri:List<Programer>):Map<String,Double>{
+    return programeri.flatMap{i->i.jezici.map{it to i.godineIskustva}}
+        .groupBy { it.first }.mapValues { it.value.map{j->j.second}.average() }
+}
+
+fun prosjecnoIskustvoVerzija2(programeri:List<Programer>):Map<String,Double> {
+    val suma = mutableMapOf<String, Double>()
+    val brojac = mutableMapOf<String, Int>()
+
+    for (i in programeri) {
+        for (j in i.jezici) {
+            suma[j] = suma.getOrDefault(j, 0.0) + i.godineIskustva
+            brojac[j] = brojac.getOrDefault(j, 0) + 1
+        }
+    }
+    val prosjek = mutableMapOf<String, Double>()
+    for (j in suma.keys) {
+        prosjek[j] = suma[j]!! / brojac[j]!!
+    }
+    return prosjek
+}
+
 
 
